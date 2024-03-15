9186e47958a36d06422ce41fc2c896b64a3e8b2e
Sergey Lyapustin <s.lyapustin@gmail.com>
2024-03-15 12:17:22 +0100

    Provided description and types for variables.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,16 @@
-variable "do_token" {}
-variable "ssh_fingerprint" {}
-variable "domain_name" {}
-variable "private_key_path" {}
+variable "do_token" {
+    description = "Digital Ocean API token (https://cloud.digitalocean.com/settings/api/tokens)"
+    type = string
+}
+variable "ssh_fingerprint" {
+    description = "SSH fingerprint. Run 'ssh-keygen -E md5 -lf ~/.ssh/id_rsa.pub' to get it"
+    type = string
+}
+variable "domain_name" {
+    description = "Domain name for application"
+    type = string
+}
+variable "private_key_path" {
+    description = "Path to private key (e.g. ~/.ssh/id_rsa)"
+    type = string
+}
